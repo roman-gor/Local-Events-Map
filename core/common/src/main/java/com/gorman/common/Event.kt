b0a0738dce51e849f58dd064f1c0e066c1d279ab
Roman-Gorbachev <rgorbachev@scand.com>
2026-01-13 11:51:21 +0300

    Add Event.kt

--- core/common/src/main/java/com/gorman/common/Event.kt
+++ core/common/src/main/java/com/gorman/common/Event.kt
@@ -1,2 +1,15 @@
-package com.gorman.common 
+package com.gorman.common
 
+data class Event (
+    val localId: Int = 0,
+    val remoteId: String = "",
+    val name: String? = null,
+    val description: String? = null,
+    val address: String? = null,
+    val coordinates: String,
+    val ling: String? = null,
+    val photo: String? = null,
+    val category: String? = null,
+    val price: String? = null,
+    val isFavourite: Boolean = false
+)
