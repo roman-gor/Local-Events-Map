6e992497bd2626aa6c957328d8a3d0a86bc3bb5d
Roman-Gorbachev <rgorbachev@scand.com>
2026-01-14 19:29:08 +0300

    Started with permissions

--- feature/events/src/main/java/com/gorman/events/ui/permissions/LocationPermissionsHandler.kt
+++ feature/events/src/main/java/com/gorman/events/ui/permissions/LocationPermissionsHandler.kt
@@ -15,8 +15,7 @@ fun LocationPermissionsHandler(
     val locationPermissionsState = rememberMultiplePermissionsState(
         permissions = listOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            Manifest.permission.ACCESS_COARSE_LOCATION
         )
     )
     when {
