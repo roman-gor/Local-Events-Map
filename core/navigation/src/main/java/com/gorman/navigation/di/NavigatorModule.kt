8f6321bd68498dcca4423da1c7c8c81917703d0f
Roman <romangorbachev2006@gmail.com>
2026-01-28 16:44:39 +0300

    Setup navigation with Navigator and NavigatorModule

---
+++ core/navigation/src/main/java/com/gorman/navigation/di/NavigatorModule.kt
@@ -0,0 +1,17 @@
+package com.gorman.navigation.di
+
+import com.gorman.feature.events.api.HomeScreenNavKey
+import com.gorman.navigation.navigator.Navigator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+
+@Module
+@InstallIn(ActivityRetainedComponent::class)
+object NavigatorModule {
+    @Provides
+    @ActivityRetainedScoped
+    fun provideNavigator() : Navigator = Navigator(startDestination = HomeScreenNavKey)
+}
