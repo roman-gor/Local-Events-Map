bbb5ea8cf0efc4f8ce45093476e5c6deeccfefe6
Roman-Gorbachev <rgorbachev@scand.com>
2026-02-10 17:44:58 +0300

    Merge from events-map

--- feature/events/impl/src/main/java/com/gorman/feature/events/impl/ui/viewmodels/MapViewModel.kt
+++ feature/events/impl/src/main/java/com/gorman/feature/events/impl/ui/viewmodels/MapViewModel.kt
@@ -203,7 +203,6 @@ class MapViewModel @Inject constructor(
                     syncEvents()
                 }
             }
-            is ScreenUiEvent.OnNavigateToDetailsScreen -> {}
             is ScreenUiEvent.OnNavigateToDetailsScreen -> {
                 navigator.goTo(DetailsScreenNavKey(event.event.id))
             }
