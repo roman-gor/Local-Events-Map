aababd2100f164c0c4b3b6f4dcbab7669ca143b4
Roman-Gorbachev <rgorbachev@scand.com>
2026-02-12 18:24:20 +0300

    Add animation in Details Screen

--- feature/details-event/impl/src/main/java/com/gorman/feature/details/impl/navigation/FeatureDetailsEntryBuilder.kt
+++ feature/details-event/impl/src/main/java/com/gorman/feature/details/impl/navigation/FeatureDetailsEntryBuilder.kt
@@ -1,5 +1,9 @@
 package com.gorman.feature.details.impl.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -7,12 +11,21 @@ import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.ui.NavDisplay
 import com.gorman.feature.details.api.DetailsScreenNavKey
 import com.gorman.feature.details.impl.ui.screens.DetailsEventScreenEntry
 import com.gorman.feature.details.impl.ui.viewmodels.DetailsViewModel
 
 fun EntryProviderScope<NavKey>.featureDetailsEntryBuilder() {
-    entry<DetailsScreenNavKey> { key ->
+    entry<DetailsScreenNavKey>(
+        metadata = NavDisplay.predictivePopTransitionSpec {
+            fadeIn(animationSpec = tween(300)) togetherWith
+                fadeOut(animationSpec = tween(300))
+        } + NavDisplay.popTransitionSpec {
+            fadeIn(animationSpec = tween(300)) togetherWith
+                fadeOut(animationSpec = tween(300))
+        }
+    ) { key ->
         val viewModel = hiltViewModel<DetailsViewModel, DetailsViewModel.Factory>(
             creationCallback = { factory ->
                 factory.create(key)
